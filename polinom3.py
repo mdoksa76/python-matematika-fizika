c25ca2b895462b4a60134f7075d326c151a366e4
mdoksa76 <122205220+mdoksa76@users.noreply.github.com>
2023-04-12 20:18:43 +0200

    Update polinom3.py

--- polinom3.py
+++ polinom3.py
@@ -6,12 +6,12 @@ import datetime
 
 def graf(a, b, c, d):
     x = range(-100,101,1)
-    y = [a * xi**3 + b*xi**2 + c*xi +d for xi in x]
+    y = [a * xi**3 + b * xi**2 + c * xi + d for xi in x]
     return x, y
 
 def main():
     koeficijenti = []
-    i=0
+    i = 0
     filename_txt = "graf-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
     filename_html = "graf-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".html"
     file_path_txt = os.path.join(os.getcwd(), filename_txt)
@@ -23,16 +23,16 @@ def main():
             b = float(input("Unesi koeficijent b = "))
             c = float(input("Unesi koeficijent c = "))
             d = float(input("Unesi koeficijent d = "))
-            koeficijenti.append((a,b,c,d))
+            koeficijenti.append((a, b, c, d))
         except ValueError:
             print("Pogrešan unos, ponovi!")
             continue
-        i=i+1
+        i = i + 1
         x, y = graf(a, b, c, d)
         plt.plot(x, y, label = koeficijenti[i-1])
-        plt.title("Graf funkcije y = a * x^3 + b * x^2 +c * x + d")
-        plt.xlim(-100,100)
-        plt.ylim(-50,50)
+        plt.title("Graf funkcije y = a * x^3 + b * x^2 + c * x + d")
+        plt.xlim(-100, 100)
+        plt.ylim(-50, 50)
         plt.vline(0)
         plt.hline(0)
         plt.xlabel("x")
